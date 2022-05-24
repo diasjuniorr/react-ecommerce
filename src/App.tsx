5cea74cd8728094975d63219d5d5f09b0b1f5378
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-05-24 09:44:04 +0200

    add sig-in route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import Home from "./routes/home/home";
 import Navigation from "./routes/navigation/navigation";
+import Home from "./routes/home/home";
+import SignIn from "./routes/sign-in/sign-in.component";
 import { Routes, Route} from "react-router-dom";
+import Shop from "./routes/shop/shop.component";
 
 
 
@@ -9,6 +11,8 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
