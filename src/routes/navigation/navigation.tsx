bad0aec4ee2935bc719d5cfc3589e2afa50d441d
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-05-25 17:31:33 +0200

    add create user from auth function

--- src/routes/navigation/navigation.tsx
+++ src/routes/navigation/navigation.tsx
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
-import {ReactComponent as CrownLogo} from "../../assets/crown.svg";
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
 
 import "./navigation.styles.scss";
 
@@ -9,14 +9,14 @@ const Navigation = () => {
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-            <CrownLogo className="logo"/>
+          <CrownLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
-            Shop
+            SHOP
           </Link>
           <Link className="nav-link" to="/sign-in">
-           Sign-in 
+            SIGN-IN
           </Link>
         </div>
       </div>
--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -1,7 +1,16 @@
+import {signinWithgooglePopup, createUserDocFromAuth} from "../../utils/firebase/firebase";
+
 const SignIn = () => {
+    const signInWithGoogle = async () => {
+        const response = await signinWithgooglePopup();
+        const user = await createUserDocFromAuth(response.user);
+        console.log("user", user);
+    }
+
     return (
         <div>
             <h1>Sign in page</h1>
+            <button onClick={signInWithGoogle}>Sign in</button>
         </div>
     )
 }
