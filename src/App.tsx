f3938cabb9b6099ced9204d75e66b39fdf002982
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-05-27 09:31:34 +0200

    add sign-up route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { Routes, Route} from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 import SignIn from "./routes/sign-in/sign-in.component";
-import { Routes, Route} from "react-router-dom";
 import Shop from "./routes/shop/shop.component";
+import SignUp from "./routes/sign-up/sign-up.component";
 
 
 
@@ -13,6 +14,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
       </Route>
     </Routes>
   );
