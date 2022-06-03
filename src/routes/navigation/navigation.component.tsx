0717d9f44ec947045eb65583d1f21cf6e6c590a9
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-03 09:57:18 +0200

    add signout handler

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,12 +1,18 @@
 import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
-import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
+import {userSignOut} from "../../utils/firebase/firebase.utils";
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
 
 import "./navigation.styles.scss";
 
 const Navigation = () => {
   const userContext = useContext(UserContext);
+
+  const signOutHandler = async() => {
+    await userSignOut();
+    userContext?.setUser(null);
+  }
 
   return (
     <Fragment>
@@ -18,9 +24,13 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className="nav-link" to="/auth">
-           {userContext?.user ? "SIGN OUT" : "SIGN IN"} 
-          </Link>
+          {userContext?.user ? (
+            <span className="nav-link" onClick={signOutHandler}>SIGN OUT</span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+               SIGN IN
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
