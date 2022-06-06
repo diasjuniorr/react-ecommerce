c7dd04cd764f6596b6e4de2c2240fb427ef74751
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-06 10:06:23 +0200

    refact cart icon

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -1,21 +1,20 @@
 import { useContext } from "react";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg"
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { CartContext } from "../../contexts/cart.context";
-import "./cart-icon.styles.scss"
+import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-    const cartContext = useContext(CartContext);
+  const cartContext = useContext(CartContext);
 
-    const handleToggleCartDropdown = () => {
-        cartContext.setIsOpen(!cartContext.isOpen);
-    }
+  const handleToggleCartDropdown = () =>
+    cartContext.setIsOpen(!cartContext.isOpen);
 
-    return (
-        <div className="cart-icon-container" onClick={handleToggleCartDropdown}>
-            <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">0</span>
-        </div>
-    )
-}
+  return (
+    <div className="cart-icon-container" onClick={handleToggleCartDropdown}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">0</span>
+    </div>
+  );
+};
 
-export default CartIcon
+export default CartIcon;
