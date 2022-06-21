4a13d0aecb5bfd290218cb5709758cea69afb266
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-21 14:36:01 +0100

    add go to checkout button

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
@@ -6,7 +7,13 @@ import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
   const cartContext = useContext(CartContext);
-  const { cartItems } = cartContext;
+  const { cartItems, setIsOpen } = cartContext;
+  const navigate = useNavigate();
+
+  const handlerGoToCheckout = () => {
+    setIsOpen(false);
+    navigate("/checkout");
+  };
 
   return (
     <div className="cart-dropdown-container">
@@ -15,7 +22,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>CHECKOUT</Button>
+      <Button onClick={handlerGoToCheckout}>CHECKOUT</Button>
     </div>
   );
 };
