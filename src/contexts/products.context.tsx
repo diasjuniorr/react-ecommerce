25700d7d0916989670118ef7c94eae5707c08b16
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-24 14:54:40 +0200

    add get products from firestore function

--- src/contexts/products.context.tsx
+++ src/contexts/products.context.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-const defaultProducts = require("../shop-data.json")
+import React, { useEffect, useState } from "react";
+import { getCategoriesAndDocuemnts } from "../utils/firebase/firebase.utils";
+const defaultProducts = require("../shop-data.json");
 
 interface Props {
   children: React.ReactNode;
@@ -13,17 +14,36 @@ interface Product {
 }
 
 interface ProductsContextProps {
-    products: Product[];
-    setProducts: React.Dispatch<React.SetStateAction<Product[]>>;
+  products: Product[];
+  setProducts: React.Dispatch<React.SetStateAction<Product[]>>;
 }
 
-export const ProductsContext = React.createContext<ProductsContextProps | null>(null);
+export const ProductsContext = React.createContext<ProductsContextProps | null>(
+  null
+);
 
 export const ProductsProvider: React.FC<Props> = ({ children }) => {
   const [products, setProducts] = useState<Product[]>(defaultProducts);
   const value = { products, setProducts };
 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const categories = await getCategoriesAndDocuemnts();
+      const products = [];
+
+      for (let key in categories) {
+        products.push(...categories[key]);
+      }
+
+      setProducts(products);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
-    <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
   );
 };
