210d5b88e82e273b72fd23f713cfce26325e2d9e
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-28 10:53:35 +0200

    refact preview category component

---
+++ src/components/preview-category/preview-category.tsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import { CategoriesMap } from "../../shared/interfaces/category.interface";
+import ProductCard from "../product-card/product-card.component";
+
+import "./preview-category.styles.scss";
+
+interface PreviewCategoryProps {
+  categoriesMap: CategoriesMap;
+}
+
+const PreviewCategory: React.FC<PreviewCategoryProps> = ({ categoriesMap }) => {
+  const { addItemToCart } = useContext(CartContext);
+
+  return (
+    <>
+      {Object.keys(categoriesMap).map((key) => {
+        const category = categoriesMap[key];
+        return (
+          <div key={key}>
+            <h2 className="category-title">
+              <span>{key}</span>
+            </h2>
+            <div className="products-container">
+              {category.slice(0, 4).map((product) => (
+                <ProductCard
+                  product={product}
+                  key={product.id}
+                  addItemToCart={addItemToCart}
+                />
+              ))}
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default PreviewCategory;
--- src/routes/shop/shop.component.tsx
+++ src/routes/shop/shop.component.tsx
@@ -1,41 +1,17 @@
 import { useContext } from "react";
-import ProductCard from "../../components/product-card/product-card.component";
-import { CartContext } from "../../contexts/cart.context";
+import PreviewCategory from "../../components/preview-category/preview-category";
 import {
   CategoriesContext,
   CategoriesContextProps,
 } from "../../contexts/categories.context";
-import "./shop.styles.scss";
 
 const Shop = () => {
   const categoriesContext = useContext(
     CategoriesContext
   ) as CategoriesContextProps;
-  const cartContext = useContext(CartContext);
   const { categoriesMap } = categoriesContext;
-  const { addItemToCart } = cartContext;
 
-  return (
-    <>
-      {Object.keys(categoriesMap).map((key) => {
-        const category = categoriesMap[key];
-        return (
-          <div key={key}>
-            <h2 className="category-title">{key}</h2>
-            <div className="products-container">
-              {category.slice(0, 4).map((product) => (
-                <ProductCard
-                  product={product}
-                  key={product.id}
-                  addItemToCart={addItemToCart}
-                />
-              ))}
-            </div>
-          </div>
-        );
-      })}
-    </>
-  );
+  return <PreviewCategory categoriesMap={categoriesMap} />;
 };
 
 export default Shop;
