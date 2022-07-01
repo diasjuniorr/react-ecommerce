512f7514f8954e9c297081f3f9e72f1c28c5fc1c
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-07-01 10:01:44 +0200

    refact to use style components

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -1,6 +1,11 @@
 import { CartItem, Product } from "../../shared/interfaces/products.interface";
 import Button from "../button/button.component";
-import "./product-card.styles.scss";
+import {
+  Footer,
+  Name,
+  Price,
+  ProductCardContainer,
+} from "./product-card.styles";
 
 interface ProductCardProps {
   product: Product;
@@ -14,16 +19,16 @@ const ProductCard: React.FC<ProductCardProps> = ({
   const addProductToCart = () => addItemToCart({ ...product, quantity: 1 });
 
   return (
-    <div className="product-card-container">
+    <ProductCardContainer>
       <img src={product.imageUrl} alt={product.name} />
-      <div className="footer">
-        <span className="name">{product.name}</span>
-        <span className="price">{product.price}</span>
-      </div>
+      <Footer>
+        <Name>{product.name}</Name>
+        <Price>{product.price}</Price>
+      </Footer>
       <Button className=" inverted button" onClick={() => addProductToCart()}>
         ADD TO CARD
       </Button>
-    </div>
+    </ProductCardContainer>
   );
 };
 
